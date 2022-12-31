19f2de97398b79fd909fa5b7b08ada7d9f7a131d
Darby Smith <newman.darby2@gmail.com>
2022-12-31 18:25:25 -0500

    Remove comments and refactor spacing

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,16 @@ Rails.application.routes.draw do
   root 'landing#index'
 
   resources :users, only: [:new, :create] 
-    # resources :discover, only: [:index]
-    resources :movies, only: [:index, :show] do
-      resources :viewing_parties, only: [:new, :create]
-    end
-  # end
-
   resources :register, only: [:new, :create], controller: "users"
   get '/dashboard', to: 'users#show'
-  
-  get '/discover', to: 'discover#index'
 
-  # get '/movies', to: 'movies#index'
-  # get '/movies/:id', to: 'movies#show'
+  resources :movies, only: [:index, :show] do
+    resources :viewing_parties, only: [:new, :create]
+  end
+
+  get '/discover', to: 'discover#index'
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-
-  # get '/movies/:id/viewing_parties/new', to: 'viewing_parties#new'
-  # post '/movies/:id/viewing_parties', to: 'viewing_parties#create'
 end
